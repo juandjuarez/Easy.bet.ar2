4000cd5a263cfd461a06d13112037a1062b852ac
juandjuarez <juanmajuarez@live.com.ar>
2024-10-18 11:20:52 -0300

    Delete Easybet directory

--- Easybet/Public/scripts.js
+++
@@ -1,5 +0,0 @@
-// Función para mostrar la segunda página cuando se hace clic en el botón
-function mostrarPagina2() {
-    document.getElementById('page1').style.display = 'none';  // Oculta la página 1
-    document.getElementById('page2').style.display = 'block';  // Muestra la página 2
-}
